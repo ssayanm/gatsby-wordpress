46f1e2f0aa60ee940819688effaffab7ff0fed6e
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-02-24 11:37:14 +0530

    added templates for pages and posts

---
+++ src/templates/page.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react"
+import { Link, graphql } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+class Page extends Component {
+  render() {
+    const StaticPage = this.props.data.wordpressPage
+
+    return (
+      <>
+        <Layout>
+          <SEO title="Home" />
+          <Link to="/page-2/">Go to page 2</Link>
+          <h1>{StaticPage.title}</h1>
+          <div>{StaticPage.content}</div>
+        </Layout>
+      </>
+    )
+  }
+}
+
+export default Page
+
+export const pageQuery = graphql`
+  query($id: String!) {
+    wordpressPage(id: { eq: $id }) {
+      title
+      content
+    }
+    site {
+      id
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
